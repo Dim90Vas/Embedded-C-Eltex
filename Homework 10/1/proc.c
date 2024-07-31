20446f78137d7e908ae75c5bdcedab455515cd94
Dim90Vas <48448669+Dim90Vas@users.noreply.github.com>
2024-07-31 20:07:04 +0700

    Update proc.c

--- Homework 10/1/proc.c
+++ Homework 10/1/proc.c
@@ -5,14 +5,15 @@
 #include <sys/wait.h>
 
 void main(void) {
-	int pid2, st;
+	pid_t pid;
+	int st;
 	printf("Process 1, pid=%d\n",getpid());
-	pid2=fork();
-	if(pid2==0) {
+	pid=fork();
+	if(pid==0) {
 		printf("Process 2, pid=%d\n",getpid());
 		printf("Process 2: end\n");
 	} else {
-		if(pid2<0) printf("Cann't create process 2: error %d\n",pid2);
+		if(pid<0) printf("Cann't create process 2: error %d\n",pid);
 		wait(&st);
 		printf("Status = %d\n",WEXITSTATUS(st));
 		printf("Process 1: end\n");
